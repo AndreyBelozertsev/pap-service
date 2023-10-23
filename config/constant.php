674e39ddfe4930876793507ecfd2d4b777c4424c
AndreyBelozertsev <andrey.belozertsev@gmail.com>
2023-10-23 18:33:31 +0300

    CreateSourceFieldAndSendTelegramIdInAmoCRM

--- config/constant.php
+++ config/constant.php
@@ -1,13 +1,13 @@
 <?php 
 
 return [
-    'telegram_group_id' => '-1001570080663',  
-    'telegram_group_link' => 'https://t.me/+Ybl8epmC-qZiZDgy',
-    'telegram_source_expression' => 'source-',
-    'amo_crm_lead_tag_name' => 'воронка веб',
-    'amo_crm_contact_tag_name' => 'воронка веб',
-    'amo_crm_pipeline_id' => 7326902,
-    'amo_crm_lead_status' => 60994990,
-    'amo_crm_lead_status_sort' => 40,
-    'amo_crm_telegram_id_field_id' => 582701,
+    'telegram_group_id' => env('CONSTANT_TELEGRAM_GROUP_ID'),  
+    'telegram_group_link' => env('CONSTANT_TELEGRAM_GROUP_LINK'),
+    'telegram_source_expression' => env('CONSTANT_TELEGRAM_SOURCE_EXPRESSION'),
+    'amo_crm_lead_tag_name' => env('CONSTANT_AMO_CRM_LEAD_TAG_NAME'),
+    'amo_crm_contact_tag_name' => env('CONSTANT_AMO_CRM_CONTACT_TAG_NAME'),
+    'amo_crm_pipeline_id' => env('CONSTANT_AMO_CRM_PIPELINE_ID'),
+    'amo_crm_lead_status' => env('CONSTANT_AMO_CRM_LEAD_STATUS'),
+    'amo_crm_lead_status_sort' => env('CONSTANT_AMO_CRM_STATUS_SORT'),
+    'amo_crm_telegram_id_field_id' => env('CONSTANT_AMO_CRM_TELEGRAM_ID_FIELD_ID'),
 ];
